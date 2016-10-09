ec80a669d6ffd4e913e3cc160b3f7a9193696647
ztong0078141 <ztong0078141@gmail.com>
2016-10-09 14:36:28 +0800

    initial git

---
+++ app/src/main/java/com/example/ztong/gitexample/GitExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.ztong.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example);
+    }
+}
